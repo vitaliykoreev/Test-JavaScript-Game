2eafb0f20e950a6c2f8ebeb3a4dc37fa2fc0ef6a
Vitalii Koreiev <vitaliykoreev@gmail.com>
2020-02-22 23:24:06 +0200

    Added Pause Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,37 +13,55 @@ window.Config = {
 window.addEventListener('DOMContentLoaded', () => {
     let world = document.getElementById("scene"),
         gameElement = document.getElementById("game"),
-        overlay = document.getElementById("overlay");
+        overlay = document.getElementById("overlay"),
+        playPauseButtons = document.querySelectorAll("[data-button-play]"),
+        initialPlayButton = document.getElementById("button-play");
 
-    let isPaused = true;
-    let startTimestamp = performance.now();
+    window.isPaused = true; // Global variable for play/pause status
+
+    gameElement.classList.add("paused");
 
     let game = requestAnimationFrame(loop);
+
+    function pause() {
+        if (!window.isPaused) {
+            cancelAnimationFrame(game);
+            gameElement.classList.add("paused");
+            initialPlayButton.innerText = "Continue";
+        } else {
+            game = requestAnimationFrame(loop);
+            gameElement.classList.remove("paused");
+        }
+
+        window.isPaused = !window.isPaused;
+    }
+
+    // Play/Pause Buttons Handler
+    playPauseButtons.forEach((button) => {
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            pause();
+        });
+    });
 
     let Jane = new Character( {width: 64, height: 153}, {x: 100, y: 300}, 9, 1, {width: 50, height: 140, x: 5});
     Jane.render();
 
-    let ground = new Ground(window.innerWidth, 100, {x: 0, y: 0});
-    ground.render();
+    // Game Objects Array
+    let blocks = [
+        new Ground(window.innerWidth, 100, {x: 0, y: 0}), // Ground block
+        new Block(200, 50, {x: 450, y: 150}),
+        new Block(200, 50, {x: 1250, y: 120}),
+        new Block(350, 50, {x: 700, y: 280}),
+        new Block(50, 50, {x: 600, y: 210}),
+        new Pipe(70, 70, {x: 100, y: 100}),
+        new Pipe(70, 120, {x: 170, y: 100}),
+        new Block(50, 500, {x: -40, y: 100}), // Left wall
+        new Block(50, 500, {x: window.innerWidth - 10, y: 100}) // Right wall
+    ];
 
-    let leftWall = new Block(50, 500, {x: -40, y: 100});
-    leftWall.render();
-    let rightWall = new Block(50, 500, {x: window.innerWidth - 10, y: 100});
-    rightWall.render();
-
-    let block1 = new Block(200, 50, {x: 350, y: 150});
-    block1.render();
-
-    let block4 = new Block(200, 50, {x: 1250, y: 120});
-    block4.render();
-
-    let block2 = new Block(350, 50, {x: 700, y: 280});
-    block2.render();
-
-    let block3 = new Pipe(70, 70, {x: 100, y: 100});
-    block3.render();
-
-    let blocks = [ground, block1, block2, block3, block4, leftWall, rightWall];
+    // Initial Render Game Objects
+    blocks.forEach(block => block.render());
 
     document.addEventListener('keydown', (event) => {
         if (event.code === 'ArrowRight') {
@@ -61,24 +79,15 @@ window.addEventListener('DOMContentLoaded', () => {
         } else if (event.code === 'Space') {
             Jane.jump();
         } else if (event.code === "Escape") {
-            if (!isPaused) {
-                cancelAnimationFrame(game);
-                gameElement.classList.add("paused");
-            } else {
-                game = requestAnimationFrame(loop);
-                gameElement.classList.remove("paused");
-            }
-
-            isPaused = !isPaused;
+            pause();
         }
     });
 
     document.addEventListener('keyup', (event) => {
-        //character.classList.remove('walk');
         Jane.stop();
     });
 
-    function loop(timestamp) {
+    function loop() {
         Jane.update(blocks);
         Jane.render();
 
